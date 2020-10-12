3970454f935da2688bd974978ec20ece286cd96e
rp <kidsman9049@gmail.com>
2020-10-13 00:50:54 +0800

    edited BlockPi/src/msg/js/en.js and added blocks/action.js

---
+++ blocks/action.js
@@ -0,0 +1,18 @@
+'use strict';
+
+goog.provide('Blockly.Blocks.action'); // Deprecated.
+goog.provide('Blockly.Constants.Action');
+
+Blockly.Constants.Action.HUE = 90;
+
+Blockly.defineBlocksWithJsonArray(
+  [{
+    "type": "move_front",
+    "message0": "%{BKY_MOVE_FRONT_TITLE}",
+    "inputsInline": true,
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": "%{BKY_ACTION_HUE}",
+    "tooltip": "%{BKY_MOVE_FRONT_TOOLTIP}",
+    "helpUrl": ""
+  },
